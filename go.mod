3735f057df30d6c81b348f59cdaf6bbded8b9ee0
Kai O'Reilly <koreilly5297@gmail.com>
2023-08-17 15:13:44 -0700

    added key, palette, and tones

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/goki/matcolor
+
+go 1.19
+
+require github.com/goki/cam v0.0.0-20230817202126-32cd3fa76bf1
+
+require (
+	github.com/goki/ki v1.1.15 // indirect
+	github.com/goki/mat32 v1.0.17 // indirect
+	golang.org/x/image v0.11.0 // indirect
+)
--- key.go
+++ key.go
@@ -1,6 +1,33 @@
+// Copyright (c) 2023, The GoKi Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package matcolor
+
+import "image/color"
 
 // Key contains the set of key colors used to generate
 // a [Scheme] and [Palette]
 type Key struct {
+
+	// the primary accent key color
+	Primary color.RGBA `desc:"the primary accent key color"`
+
+	// the secondary accent key color
+	Secondary color.RGBA `desc:"the secondary accent key color"`
+
+	// the tertiary accent key color
+	Tertiary color.RGBA `desc:"the tertiary accent key color"`
+
+	// the error accent key color
+	Error color.RGBA `desc:"the error accent key color"`
+
+	// the neutral key color used to generate surface and surface container colors
+	Neutral color.RGBA `desc:"the neutral key color used to generate surface and surface container colors"`
+
+	// the neutral variant key color used to generate surface variant and outline colors
+	NeutralVariant color.RGBA `desc:"the neutral variant key color used to generate surface variant and outline colors"`
+
+	// an optional map of custom accent key colors
+	Custom map[string]color.RGBA `desc:"an optional map of custom accent key colors"`
 }
