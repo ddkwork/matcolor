261eae3d65c9f386ea250958f9df0f13dba8f89a
Kai O'Reilly <koreilly5297@gmail.com>
2023-10-22 13:36:24 -0700

    updated version to v0.9.17

--- version.go
+++ version.go
@@ -4,9 +4,9 @@ package matcolor
 
 const (
 	// Version is the version of this package being used
-	Version = "v0.9.16"
+	Version = "v0.9.17"
 	// GitCommit is the commit just before the latest version commit
-	GitCommit = "63085ba"
+	GitCommit = "496a141"
 	// VersionDate is the date-time of the latest version commit in UTC (in the format 'YYYY-MM-DD HH:MM', which is the Go format '2006-01-02 15:04')
-	VersionDate = "2023-10-22 20:24"
+	VersionDate = "2023-10-22 20:36"
 )
