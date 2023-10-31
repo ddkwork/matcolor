e25da7ae0efd39d3ebc64dcc136aff45976d2d5a
Kai O'Reilly <koreilly5297@gmail.com>
2023-10-31 16:09:18 -0700

    updated version to v0.9.20

--- go.mod
+++ go.mod
@@ -2,10 +2,10 @@ module goki.dev/matcolor
 
 go 1.21.0
 
-require goki.dev/cam v0.9.31
+require goki.dev/cam v0.9.32
 
 require (
-	goki.dev/enums v0.9.34 // indirect
-	goki.dev/mat32/v2 v2.0.0-dev0.0.9 // indirect
+	goki.dev/enums v0.9.35 // indirect
+	goki.dev/mat32/v2 v2.0.0-dev0.0.10 // indirect
 	golang.org/x/image v0.13.0 // indirect
 )
--- version.go
+++ version.go
@@ -4,9 +4,9 @@ package matcolor
 
 const (
 	// Version is the version of this package being used
-	Version = "v0.9.19"
+	Version = "v0.9.20"
 	// GitCommit is the commit just before the latest version commit
-	GitCommit = "62ced23"
+	GitCommit = "8dd5961"
 	// VersionDate is the date-time of the latest version commit in UTC (in the format 'YYYY-MM-DD HH:MM', which is the Go format '2006-01-02 15:04')
-	VersionDate = "2023-10-31 23:05"
+	VersionDate = "2023-10-31 23:09"
 )
